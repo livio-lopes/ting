842006a967e5e429fbf8f41d3a3cc5d2d8ad2951
Lívio Lopes <antonio.lvio760@gmail.com>
2023-09-28 21:12:16 -0300

    feat(file_management): implement function txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    TXT_EXTENSION = ".txt"
+    if path_file.endswith(TXT_EXTENSION):
+        try:
+            with open(path_file) as file:
+                content = file.read()
+                content = content.split("\n")
+            return content
+        except FileNotFoundError:
+            sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    else:
+        sys.stderr.write("Formato inválido")
