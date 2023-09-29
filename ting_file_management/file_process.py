ae08e7ca67b91543dab70c20d9f6e78fb224b6c1
Lívio Lopes <antonio.lvio760@gmail.com>
2023-09-29 12:48:15 -0300

    feat(file_process): implemente function process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,31 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    extract = txt_importer(path_file)
+    file_in_queue = None
+    for index in list(range(instance.__len__())):
+        file_in_queue = (
+            instance.search(index)
+            if instance.search(index)["nome_do_arquivo"] == path_file
+            else None
+        )
+    if extract and not file_in_queue:
+        transform = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(extract),
+            "linhas_do_arquivo": extract,
+        }
+        # loading
+        instance.enqueue(transform)
+        sys.stdout.write(f"{transform}")
+        # print(sys.stdout)
+
+
+# fila_do_bode = Queue()
+# process('statics/arquivo_teste.txt', fila_do_bode)
 
 
 def remove(instance):
