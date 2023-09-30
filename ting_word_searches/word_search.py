0c7f610b24c66e554dbfe04c96f8aae1ee001b42
Lívio Lopes <antonio.lvio760@gmail.com>
2023-09-29 23:28:58 -0300

    fix(eslint): resolves issues of the flake8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -7,11 +7,12 @@ def exists_word(word, instance):
         transform["palavra"] = word
         for index, phrase in enumerate(data["linhas_do_arquivo"]):
             if word.lower() in phrase.lower():
-                transform["ocorrencias"].append({"linha": index+1})
-        if len(transform['ocorrencias']) == 0:
+                transform["ocorrencias"].append({"linha": index + 1})
+        if len(transform["ocorrencias"]) == 0:
             return load
         load.append(transform)
     return load
+
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
