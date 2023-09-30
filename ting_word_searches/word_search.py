03a04c0a5828c4dce1b2d8f5f5526c9b5691d204
Lívio Lopes <antonio.lvio760@gmail.com>
2023-09-29 23:26:41 -0300

    feat(word_search): implemented function exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,17 @@
 def exists_word(word, instance):
     """Aqui irá sua implementação"""
-
+    load = []
+    transform = {"palavra": "", "arquivo": "", "ocorrencias": []}
+    for data in instance.fifo:
+        transform["arquivo"] = data["nome_do_arquivo"]
+        transform["palavra"] = word
+        for index, phrase in enumerate(data["linhas_do_arquivo"]):
+            if word.lower() in phrase.lower():
+                transform["ocorrencias"].append({"linha": index+1})
+        if len(transform['ocorrencias']) == 0:
+            return load
+        load.append(transform)
+    return load
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
